956b9af8ac8c1181b36216ce422a14fac967ff29
Kyle Beavin <kyle.beavin@tcmcllc.com>
2021-02-26 10:29:07 -0500

    Created Spacer

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Button, Text, Input } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
     return (
       <>
-        <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-        <Button
-          title="Go to Signin"
-          onPress={() => navigation.navigate("Signin")}
-        />
-
-        <Button
-          title="Go to main flow"
-          onPress={() => navigation.navigate("mainFlow")}
-        />
+        <Spacer>
+          <Text h3>Sign Up for Tracker</Text>
+        </Spacer>
+        <Input label="Email" />
+        <Spacer />
+        <Input label="Password" />
+        <Spacer>
+          <Button title="Sign Up" onPress={() => console.log("hi")} />
+        </Spacer>
       </>
     );
 }
