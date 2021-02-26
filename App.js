8e32e77076f3a0ae1ed4ed15b1ca8547a3e763dd
Kyle Beavin <kyle.beavin@tcmcllc.com>
2021-02-26 16:03:29 -0500

    adding map and location data and new locationprovider context

--- App.js
+++ App.js
@@ -1,41 +1,48 @@
-import React from 'react';
+import React from "react";
 
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
-import AccountScreen from './src/screens/AccountScreen';
-import SigninScreen from './src/screens/SigninScreen';
-import SignupScreen from './src/screens/SignupScreen';
-import TrackCreateScreen from './src/screens/TrackCreateScreen';
-import TrackDetailScreen from './src/screens/TrackDetailScreen';
-import TrackListScreen from './src/screens/TrackListScreen';
-import { Provider as AuthProvider } from './src/context/AuthContext';
-import { setNavigator } from './src/navigationRef';
-import ResolveAuthScreen from './src/screens/ResolveAuthScreen';
+import AccountScreen from "./src/screens/AccountScreen";
+import SigninScreen from "./src/screens/SigninScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+import TrackCreateScreen from "./src/screens/TrackCreateScreen";
+import TrackDetailScreen from "./src/screens/TrackDetailScreen";
+import TrackListScreen from "./src/screens/TrackListScreen";
+import { Provider as AuthProvider } from "./src/context/AuthContext";
+import { Provider as LocationProvider } from "./src/context/LocationContext";
+import { setNavigator } from "./src/navigationRef";
+import ResolveAuthScreen from "./src/screens/ResolveAuthScreen";
 
 const switchNavigator = createSwitchNavigator({
-  ResolveAuth: ResolveAuthScreen,  
+  ResolveAuth: ResolveAuthScreen,
   loginFlow: createStackNavigator({
     Signup: SignupScreen,
-    Signin: SigninScreen
+    Signin: SigninScreen,
   }),
   mainFlow: createBottomTabNavigator({
     trackListFlow: createStackNavigator({
       TrackList: TrackListScreen,
-      TrackDetail: TrackDetailScreen
+      TrackDetail: TrackDetailScreen,
     }),
     TrackCreate: TrackCreateScreen,
-    Account: AccountScreen
-  })
+    Account: AccountScreen,
+  }),
 });
 
 const App = createAppContainer(switchNavigator);
 
 export default () => {
   return (
-    <AuthProvider>
-      <App ref={(navigator) => { setNavigator(navigator) }} />
-    </AuthProvider>
-  )
-}
+    <LocationProvider>
+      <AuthProvider>
+        <App
+          ref={(navigator) => {
+            setNavigator(navigator);
+          }}
+        />
+      </AuthProvider>
+    </LocationProvider>
+  );
+};
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,16 +1,54 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import '../_mockLocation';
+import React, { useEffect, useState, useContext } from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-navigation";
+import { Text } from "react-native-elements";
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from "expo-location";
+import Map from "../components/Map";
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackCreateScreen = () => {
-    return (
-        <>
-            <Text style={{fontSize: 48}}>TrackCreateScreen</Text>
-        </>
-    );
-}
+    const { addLocation } = useContext(LocationContext);
+  const [err, setErr] = useState(null);
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermissionsAsync();
+
+      if (!granted) {
+        throw new Error("Location permission not granted");
+      }
+
+      await watchPositionAsync({
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10
+      }, (location) => {
+        addLocation(location);
+      });
+
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  return (
+    <SafeAreaView forceInset={{ top: "always" }}>
+      <Text h2>TrackCreateScreen</Text>
+      <Map />
+      {err ? <Text style={styles.err}>Please enable location services.</Text> : null}
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    
-})
+    err: {
+        color: 'red',
+    }
+});
 
 export default TrackCreateScreen;
