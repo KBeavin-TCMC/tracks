c5c0d4cfd572cc82cf6855b81475b3e6a67bdf06
Kyle Beavin <kyle.beavin@tcmcllc.com>
2021-02-26 13:28:46 -0500

    Done with Authentication.

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useContext } from "react";
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from 'react-navigation';
+import { Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
+import { Context as AuthContext } from "../context/AuthContext";
 
 const AccountScreen = () => {
-    return (
-        <>
-            <Text style={{fontSize: 48}}>AccountScreen</Text>
-        </>
-    );
-}
+  const { signout } = useContext(AuthContext);
 
-const styles = StyleSheet.create({
-    
-})
+  return (
+    <SafeAreaView forceInset={{top: 'always'}}>
+      <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+      <Spacer>
+        <Button title="Sign Out" onPress={signout} />
+      </Spacer>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
 
 export default AccountScreen;
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Button, Text, Input } from 'react-native-elements';
-import Spacer from '../components/Spacer';
+import React, { useContext } from "react";
+import { StyleSheet, View } from "react-native";
+import { NavigationEvents } from "react-navigation";
+import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
 
-const SignupScreen = ({ navigation }) => {
-    return (
-      <>
-        <Spacer>
-          <Text h3>Sign Up for Tracker</Text>
-        </Spacer>
-        <Input label="Email" />
-        <Spacer />
-        <Input label="Password" />
-        <Spacer>
-          <Button title="Sign Up" onPress={() => console.log("hi")} />
-        </Spacer>
-      </>
-    );
-}
+const SignupScreen = () => {
+  const { state, signup, clearErrorMessage } = useContext(AuthContext);
+
+  return (
+    <View style={styles.container}>
+      <NavigationEvents onWillFocus={clearErrorMessage} />
+      <AuthForm
+        headerText="Sign up for Tracker"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign Up"
+        onSubmit={signup}
+      />
+      <NavLink
+        routeName="Signin"
+        text="Already have an account? Sign in instead!"
+      />
+    </View>
+  );
+};
+
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
 
 const styles = StyleSheet.create({
-    
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 250,
+  },
+});
 
 export default SignupScreen;
